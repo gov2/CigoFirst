fbaee1ac2782310f5a82451415d311eaecc82fd4
zjugis <zgis@PC201109131534.local>
2012-10-09 19:52:08 +0800

    Initial Commit

---
+++ CigoFirst/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  CigoFirst
+//
+//  Created by zjugis on 12-10-9.
+//  Copyright (c) 2012年 cigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
